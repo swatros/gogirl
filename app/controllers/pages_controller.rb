a220894949d8670c84c4bf568e06501b946ac04a
Alejandro Gorina <alejandrogorinam@gmail.com>
2021-09-07 16:11:46 +0200

    Added buttons and input forms

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,4 +6,8 @@ class PagesController < ApplicationController
 
   def navigation
   end
+
+  def uikit
+
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,6 @@ Rails.application.routes.draw do
 
   resources :incidents, only: [:create, :index, :update]
   resources :ratings, only: [:create, :index]
+
+  get "/uikit", to: "pages#uikit"
 end
