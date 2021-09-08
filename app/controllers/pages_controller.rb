3fa4b07402f1820cfd3d8b984b718faec682412e
Loulou Goldfarb <goldfarb.loulou@gmail.com>
2021-09-08 15:09:40 +0200

    Route works

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,3 +1,5 @@
+require 'json'
+
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home, :navigation ]
 
@@ -5,7 +7,8 @@ class PagesController < ApplicationController
   end
 
   def navigation
-
+    @origin = Geocoder.search(params[:origin]).first.coordinates.to_json
+    @destination = Geocoder.search(params[:destination]).first.coordinates.to_json
   end
 
   def uikit
